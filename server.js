8e8621edf764037a3f966bb757d1b349b3acae9b
Guilherme Malfatti <guimalfatti76@gmail.com>
2018-08-16 20:57:06 -0300

    feat(add) Add item to the cart

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+var bodyParser = require('body-parser');
+var request = require('request')
 const PORT = process.env.PORT || 8080
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
+app.use(bodyParser.json());  
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
     res.render('pages/products/index');
 })
   
-app.get('/products', function(req, res) { 
-    var request = require('request')
+app.get('/products', (req, res) =>{ 
+    
     var EventEmitter = require("events").EventEmitter;
     var body = new EventEmitter();
 
@@ -34,8 +36,35 @@ app.get('/products', function(req, res) {
     body.on('update', function () {
         if(body.data != null)
             res.status(200).send(body.data)
-        else//TODO
-        res.status(500).send({})
+        else//TODO handle exception
+            res.status(200).send({})
+    });
+})
+
+app.post("/addItem", (req, res) =>{
+    var prodId = req.body.id =! null ? req.body.id : "";
+    var addItemURL = "https://shopping-cart-api-mcf.herokuapp.com/shoppingcart/items?product_id="+prodId+"&quantity=1";
+    var EventEmitter = require("events").EventEmitter;
+    var body = new EventEmitter();
+
+    request({
+        url: addItemURL,
+        method: "POST",
+        json: true
+    }, function (error, response, data){
+        if (!error && response.statusCode === 200)
+            body.data = data;
+        else
+            body.data = null;
+
+        body.emit('update');
+    });
+
+    body.on('update', function () {
+        if(body.data != null)
+            res.status(200).send(body.data)
+        else//TODO handle exception
+            res.status(200).send({error: true})
     });
 })
 
--- static/js/products.js
+++ static/js/products.js
@@ -1,17 +1,31 @@
 var ListViewModel = function() {
- 
+    this.isVisible = false;
     this.todo = function() {
         alert("TODO");
     };
     
-    this.addTocart = function() {
-        alert("TODO");
-    };
+    this.addTocart = function(item) {
+        $.ajax({
+            url: "addItem",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({
+                id: item.id
+            })
+        }).done((resp)=>{
+            if(resp.error)
+                alert('Product not added')
+            else{
+                alert('Product added')
+            }
+        })
+    }
 
     var self = this;
     this.products = ko.observableArray();
  
     this.refresh = function() {
+        //TODO show loading when no visible
         return $.getJSON("/products", function(items) {        
             self.products(items);
         });
